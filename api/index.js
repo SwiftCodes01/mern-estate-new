7f30c1a422a4bbc51b892343a50d5a893da7ba3b
Swift Media <swiftmedia90@gmail.com>
2024-08-29 14:31:16 +0100

    create a test Api route

--- api/index.js
+++ api/index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDatabase } from "./Database/connectDatabse.js";
-
-
+import userRouter from "./routes/user.route.js";
 
 const app = express();
 dotenv.config();
 
+app.use("/api/user",userRouter);
+
 app.listen(3000, () => {
-  connectDatabase()
+  connectDatabase();
   console.log("Server is running on port 3000!!!");
 });
